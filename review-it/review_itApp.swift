af91803719f4c9a3fd36103923a1160f44cc0311
Daniel Arden <daniel.arden1@gmail.com>
2022-10-02 12:38:39 +0200

    Initial Commit

---
+++ review-it/review_itApp.swift
@@ -0,0 +1,17 @@
+//
+//  review_itApp.swift
+//  review-it
+//
+//  Created by Daniel Arden on 02.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct review_itApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
