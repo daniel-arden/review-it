915807d0744cb7ffb506ef02367df10f889a5c8a
Daniel Arden <daniel.arden1@gmail.com>
2022-10-12 22:52:00 +0200

    add view scrolling to first when added or switching selected ID

--- review-it/Screens/ReviewList/ReviewListScreen.swift
+++ review-it/Screens/ReviewList/ReviewListScreen.swift
@@ -4,6 +4,14 @@ struct ReviewListScreen: View {
     @StateObject private var viewModel = ReviewListVM()
     @State private var addAppPresented = false
 
+    @State private var shouldScrollAppListToFirst = UUID()
+    @State private var shouldScrollReviewListToTop = UUID()
+
+    /// Used for hacking a way how to scroll to top in a vertical `ScrollView`
+    private static let reviewListTopViewId = "reviewListTopViewId"
+    /// Used for hacking a way how to scroll to the leading view  in a horizontal `ScrollView`
+    private static let appListFirstViewId = "appListFirstViewId"
+
     @FetchRequest(
         sortDescriptors: AppModel.sortDescriptors()
     )
@@ -12,19 +20,31 @@ struct ReviewListScreen: View {
     var body: some View {
         VStack {
             VStack(spacing: 0) {
-                ScrollView(.horizontal) {
-                    HStack {
-                        ForEach(appModels.map { AppTileModel.app($0) } ) { tileModel in
-                            AppTileView(appTileModel: tileModel, isSelected: viewModel.selectedAppId == tileModel.id ) {
-                                viewModel.selectedAppId = tileModel.id
+                ScrollView(.horizontal, showsIndicators: false) {
+                    ScrollViewReader { proxy in
+                        HStack {
+                            // FIXME: Scrolling to the first position on iOS
+                            Spacer()
+                                .frame(width: 1)
+                                .id(Self.appListFirstViewId)
+                                .onChange(of: shouldScrollAppListToFirst) { _ in
+                                    withAnimation {
+                                        proxy.scrollTo(Self.appListFirstViewId)
+                                    }
+                                }
+
+                            ForEach(appModels.map { AppTileModel.app($0) } ) { tileModel in
+                                AppTileView(appTileModel: tileModel, isSelected: viewModel.selectedAppId == tileModel.id ) {
+                                    viewModel.selectedAppId = tileModel.id
+                                }
+                            }
+
+                            AppTileView(appTileModel: .addNew, isSelected: false) {
+                                addAppPresented.toggle()
                             }
                         }
-
-                        AppTileView(appTileModel: .addNew, isSelected: false) {
-                            addAppPresented.toggle()
-                        }
+                        .padding(.horizontal)
                     }
-                    .padding(.horizontal)
                 }
                 .padding(.vertical)
 
@@ -33,12 +53,24 @@ struct ReviewListScreen: View {
             .fixedSize(horizontal: false, vertical: true)
 
             ScrollView {
-                LazyVStack(alignment: .leading) {
-                    ForEach(viewModel.reviews) {
-                        ReviewCardView(review: $0)
+                ScrollViewReader { proxy in
+                    LazyVStack(alignment: .leading) {
+                        Spacer()
+                            .frame(height: 1)
+                            .id(Self.reviewListTopViewId)
+                            .onChange(of: shouldScrollReviewListToTop) { _ in
+                                proxy.scrollTo(Self.reviewListTopViewId)
+                            }
+                            .onChange(of: viewModel.selectedAppId) { _ in
+                                shouldScrollReviewListToTop = UUID()
+                            }
+
+                        ForEach(viewModel.reviews) {
+                            ReviewCardView(review: $0)
+                        }
                     }
+                    .padding()
                 }
-                .padding()
             }
             .overlay {
                 if viewModel.reviews.isEmpty {
@@ -47,12 +79,14 @@ struct ReviewListScreen: View {
                         .foregroundColor(.secondary)
                 }
             }
+            .refreshable { viewModel.fetchReviews() }
         }
         .frame(minWidth: 400, minHeight: 400)
         .onAppear(perform: viewModel.fetchReviews)
         .sheet(isPresented: $addAppPresented) {
             AddAppScreen {
                 viewModel.selectLast()
+                shouldScrollAppListToFirst = UUID()
             }
         }
     }
