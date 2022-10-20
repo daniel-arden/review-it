4fffc3017bc22de8e1b3d0355ed67aabdc83a73d
Daniel Arden <daniel.arden1@gmail.com>
2022-10-20 17:14:08 +0200

    add about section

--- review-it/review_itApp.swift
+++ review-it/review_itApp.swift
@@ -10,11 +10,42 @@ struct review_itApp: App {
                      PersistenceController.shared.container.viewContext
                 )
         }
+        .applyCommands()
 
         #if os(macOS)
         Settings {
             SettingsScreen()
         }
+        #endif
+    }
+}
+
+private extension Scene {
+    @SceneBuilder
+    func applyCommands() -> some Scene {
+        #if os(macOS)
+        self.commands {
+            CommandGroup(replacing: .appInfo) {
+                Button("About ReviewIT") {
+                    NSApplication.shared.orderFrontStandardAboutPanel(
+                        options: [
+                            NSApplication.AboutPanelOptionKey.credits: NSAttributedString(
+                                string: "Some custom info about my app.",
+                                attributes: [
+                                    NSAttributedString.Key.font: NSFont.boldSystemFont(
+                                        ofSize: NSFont.smallSystemFontSize)
+                                ]
+                            ),
+                            NSApplication.AboutPanelOptionKey(
+                                rawValue: "Copyright"
+                            ): "© 2022 Daniel Arden"
+                        ]
+                    )
+                }
+            }
+        }
+        #else
+        self
         #endif
     }
 }
